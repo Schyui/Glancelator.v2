b775725280dd6884abeb7604b113bafbb1b06ae8
hajiireee <javier.329671@dasmarinas.sti.edu.ph>
2025-10-09 18:45:29 +0800

    settings2

---
+++ Glancelator.v1/Settings.xaml.cs
@@ -0,0 +1,36 @@
+namespace Glancelator.v1;
+
+public partial class Settings : ContentPage
+{
+	
+	public Settings()
+	{
+		InitializeComponent();
+        
+    }
+
+  
+
+    /*
+	private async void OnBackClicked(object sender, EventArgs e)
+	{
+		await Navigation.PopAsync();
+    }
+
+	private async void OnSaveClicked(object sender, EventArgs e)
+	{
+		await Navigation.PopAsync();
+    }
+
+	private void OnDarkModeToggled(object sender, ToggledEventArgs e)
+	{
+		if (e.Value)
+		{
+			Application.Current.UserAppTheme = AppTheme.Dark;
+		}
+		else
+		{
+			Application.Current.UserAppTheme = AppTheme.Light;
+		}
+    } */
+}
